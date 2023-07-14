9b3dec89f6c8bbdd343ed1375c8b6b5a09285e47
Furkan Karaağaç <furkankaraagc@gmail.com>
2023-07-14 12:03:36 +0300

    add image upload and create listings

--- src/app/components/modal/RentModal.tsx
+++ src/app/components/modal/RentModal.tsx
@@ -10,11 +10,13 @@ import { useMemo, useState } from 'react';
 import useRentModal from '@/app/hooks/useRentModal';
 
 import Modal from './Modal';
+import Counter from '../inputs/Counter';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
 import { categories } from '../navbar/Categories';
 import Input from '../inputs/Input';
 import Heading from '../Heading';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
   CATEGORY = 0,
@@ -177,6 +179,26 @@ const RentModal = () => {
           title='Share some basics about your place'
           subtitle='What amenitis do you have?'
         />
+        <Counter
+          onChange={(value) => setCustomValue('guestCount', value)}
+          value={guestCount}
+          title='Guests'
+          subtitle='How many guests do you allow?'
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue('roomCount', value)}
+          value={roomCount}
+          title='Rooms'
+          subtitle='How many rooms do you have?'
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+          value={bathroomCount}
+          title='Bathrooms'
+          subtitle='How many bathrooms do you have?'
+        />
       </div>
     );
   }
@@ -187,6 +209,10 @@ const RentModal = () => {
         <Heading
           title='Add a photo of your place'
           subtitle='Show guests what your place looks like!'
+        />
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
         />
       </div>
     );
