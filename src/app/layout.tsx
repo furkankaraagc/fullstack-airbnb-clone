fa15b0537b01763202a617687aa957da277dfde5
Furkan Karaağaç <furkankaraagc@gmail.com>
2023-07-15 17:54:23 +0300

    add search modal and favorite list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
-import './globals.css';
-import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
-import Navbar from './components/navbar/Navbar';
-import Modal from './components/modal/Modal';
-import RegisterModal from './components/modal/RegisterModal';
-import ToasterProvider from './providers/ToasterProvider';
-import LoginModal from './components/modal/LoginModal';
-import getCurrentUser from './actions/getCurrentUser';
-import RentModal from './components/modal/RentModal';
 
-export const metadata: Metadata = {
+import Navbar from '@/app/components/navbar/Navbar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import SearchModal from '@/app/components/modals/SearchModal';
+import RentModal from '@/app/components/modals/RentModal';
+
+import ToasterProvider from '@/app/providers/ToasterProvider';
+
+import './globals.css';
+import ClientOnly from './components/ClientOnly';
+import getCurrentUser from './actions/getCurrentUser';
+
+export const metadata = {
   title: 'Airbnb',
-  description: 'Airbnb clone',
+  description: 'Airbnb Clone',
 };
+
 const font = Nunito({
   subsets: ['latin'],
 });
@@ -23,16 +27,19 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const currentUser = await getCurrentUser();
+
   return (
     <html lang='en'>
       <body className={font.className}>
-        <ToasterProvider />
-        <RentModal />
-        <LoginModal />
-        <RegisterModal />
-        {/* <Modal isOpen /> */}
-        <Navbar currentUser={currentUser} />
-        {children}
+        <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <SearchModal />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className='pb-20 pt-28'>{children}</div>
       </body>
     </html>
   );
